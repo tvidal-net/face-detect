df7d3130863fa811c15cb3a48fd821343a63a463
thiago <thiago@tvidal.uk>
2020-07-17 13:56:55 +0100

    fix opencv classifier path location

--- src/main/kotlin/net/tvidal/facedetect/FaceDetection.kt
+++ src/main/kotlin/net/tvidal/facedetect/FaceDetection.kt
@@ -6,16 +6,11 @@ import javafx.application.Platform.runLater
 import javafx.beans.property.ObjectProperty
 import javafx.embed.swing.SwingFXUtils.toFXImage
 import javafx.scene.image.Image
-import org.opencv.core.Core
 import org.opencv.core.Mat
 import org.opencv.core.Point
 import org.opencv.core.Rect
 import org.opencv.core.Scalar
-import org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY
-import org.opencv.imgproc.Imgproc.cvtColor
-import org.opencv.imgproc.Imgproc.equalizeHist
-import org.opencv.imgproc.Imgproc.putText
-import org.opencv.imgproc.Imgproc.rectangle
+import org.opencv.imgproc.Imgproc.*
 import org.opencv.objdetect.CascadeClassifier
 import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_3BYTE_BGR
@@ -23,26 +18,21 @@ import java.awt.image.BufferedImage.TYPE_BYTE_GRAY
 import java.awt.image.DataBufferByte
 import java.io.FileNotFoundException
 import java.lang.System.arraycopy
-import java.lang.System.getProperty
 import java.nio.file.Files.notExists
 import java.nio.file.Paths
 
 private val PATH_FIX = Regex("^/(\\w:)")
 
-private val RESOURCE_PATH = Paths.get(getProperty("java.library.path"))
-    .also { println("RESOURCE_PATH: $it") }
-
-private const val CASCADE_CLASSIFIER_PATH = "haarcascades/haarcascade_"
+private const val CASCADE_CLASSIFIER_PATH = "/usr/share/opencv4/haarcascades/haarcascade_"
 private const val CASCADE_CLASSIFIER_EXTENSION = ".xml"
 
-private const val TEXT_FONT = Core.FONT_HERSHEY_DUPLEX
+private const val TEXT_FONT = FONT_HERSHEY_DUPLEX
 private const val TEXT_SIZE = 1.2
 private const val TEXT_MARGIN = 16.0
 private const val TEXT_THICKNESS = 2
 
 fun createCascadeClassifier(classifierName: String): CascadeClassifier {
-    val resourcePath = "$CASCADE_CLASSIFIER_PATH$classifierName$CASCADE_CLASSIFIER_EXTENSION"
-    val filePath = RESOURCE_PATH.resolveSibling(resourcePath)
+    val filePath = Paths.get("$CASCADE_CLASSIFIER_PATH$classifierName$CASCADE_CLASSIFIER_EXTENSION")
     if (notExists(filePath)) {
         throw FileNotFoundException("$filePath")
     }
@@ -74,10 +64,10 @@ fun Mat.draw(rects: Iterable<Rect>, color: Scalar, thickness: Int = 2) {
 fun Mat.subText(text: String, rect: Rect, color: Scalar) {
     val textPos = Point(rect.x + TEXT_MARGIN, rect.y + rect.height - TEXT_MARGIN)
     putText(
-        this, text, textPos,
-        TEXT_FONT,
-        TEXT_SIZE, color,
-        TEXT_THICKNESS
+            this, text, textPos,
+            TEXT_FONT,
+            TEXT_SIZE, color,
+            TEXT_THICKNESS
     )
 }
 
